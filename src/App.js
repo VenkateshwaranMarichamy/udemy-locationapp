a329e0892f2925fd8bd9a555a51ba60b7ec81cc9
venkatesh <venkateshwaranmarichamy@gmail.com>
2022-07-04 23:01:09 +0530

    Basic functional component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import SeasonDisplay from './SeasonDisplay';
 
 function App() {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
   return (
-    <div className="App">
+    <div>
      Hello react. welcome
     </div>
   );
