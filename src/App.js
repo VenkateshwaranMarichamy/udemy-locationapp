6129d84c3a352adbd1e6572035097154b87d9318
venkatesh <venkateshwaranmarichamy@gmail.com>
2022-07-06 22:44:37 +0530

    implementing with LifeCycle methos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import SeasonDisplay from './SeasonDisplay';
+import React from 'react'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
-function App() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component{
 
-  return (
-    <div>
-     Hello react. welcome
-    </div>
-  );
+  // constructor(props){
+  //   super(props);
+  //   this.state = {latitude: null, errorMessage: '' };
+  //  };
+
+  state =  {latitude: null, errorMessage: '' };
+
+  componentDidMount(){
+    console.log("App Component was rendered")
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({latitude: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
+    );
+  }
+
+  componentDidUpdate(){
+    console.log("component updated")
+  }
+  
+  renderContent(){
+    if(this.state.errorMessage && !this.state.latitude){
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if(!this.state.errorMessage && this.state.latitude){
+      return <SeasonDisplay latitude={this.state.latitude} />
+    }
+
+    return(
+      <Spinner message="Please accept the location access"/>  
+    );
+    };
+
+
+  render(){
+    return(
+      <div className="border red">{this.renderContent()}</div>
+    );
+  }
 }
 
 export default App;
